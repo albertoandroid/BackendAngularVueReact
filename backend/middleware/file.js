c854d05da7865ca12b6f4fd9aa2ab2501140c367
Alberto <info@androiddesdecero.com>
2019-11-23 14:41:31 +0100

    23

--- backend/middleware/file.js
+++ backend/middleware/file.js
@@ -11,3 +11,17 @@ const storage = multer.diskStorage({
         cb(null, filename)
     }
 })
+
+const upload = multer({
+    storage: storage,
+    fileFilter:(req, file, cb)=>{
+        if(file.mimetype = "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg"){
+            cb(null, true)
+        }else{
+            cb(null, false)
+            return cb(new Error('Solo acepta .png, .jpg, y jpeg'))
+        }
+    }
+})
+
+module.exports = upload
