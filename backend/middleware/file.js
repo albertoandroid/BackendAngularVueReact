8405fd634677fe852f851665c521fc67d1478d84
Alberto <info@androiddesdecero.com>
2019-11-23 14:35:47 +0100

    22

--- backend/middleware/file.js
+++ backend/middleware/file.js
@@ -1,3 +1,13 @@
 const multer = require('multer')
 
 const DIR = './public/'
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, DIR)
+    },
+    filename: (req, file, cb)=>{
+        const filename = Date.now() +  '-' + file.originalname.toLowerCase().split(' ').join('-')
+        cb(null, filename)
+    }
+})
