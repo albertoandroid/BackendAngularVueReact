9bf3acace5101ebd1d205efc8e66de51be3984f1
Alberto <info@androiddesdecero.com>
2019-11-23 10:28:26 +0100

    10

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 
 const user = require('./routes/user')
+const auth = require('./routes/auth')
 
 const app = express()
 app.use(express.json())
 app.use('/api/user/', user)
+app.use('/api/auth/', auth)
 
 const port = process.env.PORT || 3003
 app.listen(port, ()=> console.log('Escuchando Puerto: ' + port))
