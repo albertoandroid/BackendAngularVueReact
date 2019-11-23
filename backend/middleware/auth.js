85865129d1f32b025228025bfcecddb67b0aef9b
Alberto <info@androiddesdecero.com>
2019-11-23 11:39:51 +0100

    15

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 
 function auth(req, res, next){
     let jwtToken = req.header('Authorization')
+    if(!jwtToken) return res.status(401).send('Acceso Denegado. No hay token')
     jwtToken = jwtToken.split(' ')[1]
     if(!jwtToken) return res.status(401).send('Acceso Denegado. No hay token')
 
